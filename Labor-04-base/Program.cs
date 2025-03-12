adbe0f3eb79aca6fa202c161d00ac42f3fc12ca4
Fall1ngLight <keruloszabolcs1@gmail.com>
2025-03-12 19:38:45 +0100

    📈 progress: Labor-04-base / finished plus exercise

--- Labor-04-base/Program.cs
+++ Labor-04-base/Program.cs
@@ -16,7 +16,7 @@ internal class Program
         Console.WriteLine($"Is {numToTest1} a prime? {(pt1.IsPrime() ? "Yes" : "No")}");
         Console.WriteLine($"Is {numToTest2} a prime? {(pt2.IsPrime() ? "Yes" : "No")}");
 
-        Console.ReadLine();
+        // Console.ReadLine();
         Console.Clear();
         #endregion
 
@@ -25,6 +25,51 @@ internal class Program
 
         ArrayStatistics arrayStatistics = new ArrayStatistics();
         arrayStatistics.RunExamples();
+
+        Console.Clear();
+        // Console.ReadLine();
+        #endregion
+
+        #region 3. exercise
+        Console.WriteLine("3. exercise");
+        Stack stack = new Stack(5);
+
+        Console.WriteLine(stack.GetAllItems());
+
+        FillStack();
+
+        Console.WriteLine(stack.GetAllItems());
+        Console.WriteLine($"Is the stack empty? {(stack.Empty() ? "Yes" : "No")}");
+        Console.WriteLine($"Is the stack full? {(stack.Full() ? "Yes" : "No")}");
+
+        PopFromStack(3);
+
+        Console.WriteLine(stack.GetAllItems());
+
+        // helper methods
+        void FillStack()
+        {
+            stack.Push('a');
+            stack.Push('b');
+            stack.Push('c');
+            stack.Push('d');
+            stack.Push('q');
+        }
+
+        void PopFromStack(int numOfItems)
+        {
+            for (int i = 0; i < numOfItems; i++)
+            {
+                bool result = stack.Pop(out char item);
+
+                Console.WriteLine(
+                    result
+                        ? $"Latest item popped: {item}"
+                        : "Item could not be popped, because the stack is empty."
+                );
+            }
+        }
+
         #endregion
     }
 }
---
+++ Labor-04-base/Stack.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace Labor_04_base;
+
+public class Stack
+{
+    // fields
+    private readonly char[] _arr;
+    private int _count;
+    private int _lastIdx;
+
+    // constructors
+    public Stack(int size = 1)
+    {
+        _arr = new char[size];
+        _lastIdx = size;
+    }
+
+    // methods
+    public bool Push(char newItem)
+    {
+        if (Full())
+            return false;
+
+        // i = _count-1
+        _arr[--_lastIdx] = newItem;
+        _count++;
+        return true;
+    }
+
+    public bool Pop(out char item)
+    {
+        if (Empty())
+        {
+            item = '\0';
+            return false;
+        }
+
+        item = _arr[_lastIdx];
+        _arr[_lastIdx] = char.MinValue;
+        _lastIdx++;
+        return true;
+    }
+
+    public bool Empty() => _lastIdx == _arr.Length;
+
+    public bool Full() => _lastIdx == 0;
+
+    public string GetAllItems()
+    {
+        StringBuilder sb = new StringBuilder("Stack: \n");
+
+        for (var i = 0; i < _arr.Length; i++)
+            sb.AppendLine($"[ {(_arr[i] != char.MinValue ? _arr[i] : "")} ]");
+
+        return sb.ToString();
+    }
+
+    // properties
+    public int NumOfElements => _count;
+}
